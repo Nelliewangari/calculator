50e8698de4a75877f5df264c0b527b87b81fec89
Nellie Wangari <Nelliewangari11@gmail.com>
2019-02-13 09:30:45 +0300

    creating a calculator

---
+++ calculator/js/scripts.js
@@ -0,0 +1,7 @@
+var num = parseInt(prompt("enter number"));
+var num2 = parseInt(prompt(
+  "enter the second number"));
+  var add = function (number1, number2) {
+    return number1+number2;
+  }
+  alert(add(num, num2));
